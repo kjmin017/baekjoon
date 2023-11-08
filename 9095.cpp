fd5ee4db3517bb7b3f7009cd08f3bca2d25de382
kjmin017 <80624464+kjmin017@users.noreply.github.com>
2023-11-08 23:32:44 +0900

    Create 9095.cpp

---
+++ 9095.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[11]= {0};
+
+using namespace std;
+
+int main(void) {
+    int n;
+    int t = 0;
+    
+    dp[1]=1;
+    dp[2]=2;
+    dp[3]=4;
+    cin >> t;
+    for(int k = 0; k<t; k++){
+        cin >> n;
+        for(int i =4; i<=n; i++){
+            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+        }
+
+        cout << dp[n];
+    }
+    
+    
+
+    return 0;
+}
