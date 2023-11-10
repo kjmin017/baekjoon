cf8ea7f8dd8dc0ad4c4fba820221da47335b1ac2
kjmin017 <80624464+kjmin017@users.noreply.github.com>
2023-11-11 00:50:12 +0900

    solved

---
+++ 11399.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+    int t;
+    int sum=0;
+
+    cin >> t;
+    int numbers[t];
+    for(int i=0; i<t; i++){
+            cin >> numbers[i];
+        }
+    sort(numbers, numbers + t);
+    sum = numbers[0];
+    for(int i=1; i<t; i++){
+        numbers[i] +=numbers[i-1];
+        sum +=numbers[i];
+    }
+    cout << sum;
+    return 0;
+}
---
+++ 11726.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <iostream>
+using namespace std;
+int dp[1000]= {0};
+
+int main(void) {
+    int t;
+    int N = 0;
+    dp[0] = 1;
+    dp[1] = 2;
+    cin >> N;
+    if(N>2){
+        for(int i=2; i<N; i++){
+            dp[i] = (dp[i-2] + dp[i-1])%10007;
+        }
+    }
+    cout << dp[N-1] << endl;
+    return 0;
+}
